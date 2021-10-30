99ac656dba47f0ca9a2ec91a2a28f78dff845d1d
chronoDave <davidwolters6758@gmail.com>
2021-10-30 17:55:44 +0200

    Added sections + footer

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -1,7 +1,9 @@
 const navigation = document.querySelector('.Navigation');
 
 let isActive = window.screenY > 10;
-window.addEventListener('scroll', event => {
+
+if (isActive) navigation?.classList.add('active');
+window.addEventListener('scroll', () => {
   if (isActive !== window.scrollY > 10) {
     isActive = !!navigation?.classList.toggle('active');
   }
