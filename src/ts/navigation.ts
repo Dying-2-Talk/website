18f7a4da88ce85774a7e02bafd3ccf5c09d4509f
chronoDave <davidwolters6758@gmail.com>
2021-11-17 16:44:50 +0100

    Added mobile styling

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -1,10 +1,31 @@
+// Background
 const navigation = document.querySelector('.Navigation');
+if (navigation) {
+  let isActive = window.screenY > 10;
+  if (isActive) navigation.classList.add('active');
+  window.addEventListener('scroll', () => {
+    if (isActive !== window.scrollY > 10) {
+      isActive = !!navigation.classList.toggle('active');
+    }
+  }, { passive: true });
+}
 
-let isActive = window.screenY > 10;
+// Mobile nav
+const menuButton = navigation?.querySelector('nav > button');
+const menuList = navigation?.querySelector('nav > ul');
+if (menuButton && menuList) {
+  let open = false;
 
-if (isActive) navigation?.classList.add('active');
-window.addEventListener('scroll', () => {
-  if (isActive !== window.scrollY > 10) {
-    isActive = !!navigation?.classList.toggle('active');
-  }
-}, { passive: true });
+  menuButton.addEventListener('click', () => {
+    open = !open;
+    menuButton.classList.toggle('active');
+    menuList.classList.toggle('open');
+  }, { passive: true });
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+      menuButton.classList.remove('active');
+      menuList.classList.remove('open');
+    }
+  }, { passive: true });
+}
